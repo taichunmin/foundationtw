0b7c8bf285507b8e1361ebaff7cd3da3aa2f9de5
Finjon Kiang <kiange@gmail.com>
2015-01-13 23:48:30 +0800

    show removed files count

--- kiang/4_remove_brokens.php
+++ kiang/4_remove_brokens.php
@@ -2,9 +2,14 @@
 
 $rootPath = dirname(__DIR__);
 
+$counter = 0;
+
 foreach (glob("{$rootPath}/output/details/*/*/*.json") AS $jsonFile) {
     $json = json_decode(file_get_contents($jsonFile), true);
     if (md5(implode('', array_keys($json))) !== 'f35ba01b5874b0a155ab639b434e9d5c') {
         unlink($jsonFile);
+        ++$counter;
     }
 }
+
+echo "removed {$counter} files\n";
