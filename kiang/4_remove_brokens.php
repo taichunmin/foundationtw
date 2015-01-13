da7a0c9d804b42eb13e7549266cf20f3ee0f0a16
Finjon Kiang <kiange@gmail.com>
2015-01-13 23:31:03 +0800

    add a script to remove broken detail file

---
+++ kiang/4_remove_brokens.php
@@ -0,0 +1,10 @@
+<?php
+
+$rootPath = dirname(__DIR__);
+
+foreach (glob("{$rootPath}/output/details/*/*/*.json") AS $jsonFile) {
+    $json = json_decode(file_get_contents($jsonFile), true);
+    if (md5(implode('', array_keys($json))) !== 'f35ba01b5874b0a155ab639b434e9d5c') {
+        unlink($jsonFile);
+    }
+}
